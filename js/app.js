587e912e1eab368a72f4c71becd48bfd798fee5b
Spectrum5 <toskusergio@gmail.com>
2022-12-17 15:42:58 +0100

    Esercizio completato

--- js/app.js
+++ js/app.js
@@ -7,3 +7,10 @@ scrivi sulla pagina nomecognomecolorepreferito21
 */
 
 
+const psw = document.getElementById('my_psw');
+
+const name = prompt('Scrivi il tuo nome');
+const surname = prompt('Scrivi il tuo cognome');
+const favColor = prompt('Qual è il tuo colore preferito?');
+
+psw.innerHTML = 'Password consigliata: ' + name + surname + favColor + 21;
