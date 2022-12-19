34e8da61177656c992c8d7da161c3133abcd48f7
Spectrum5 <toskusergio@gmail.com>
2022-12-19 10:13:58 +0100

    Aggiornato esercizio completato

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
-/*
-ESERCIZIO PER OGGI:
-chiedi nome
-chiedi cognome
-chiedi colore preferito
-scrivi sulla pagina nomecognomecolorepreferito21
-*/
 
+//ESERCIZIO PER OGGI:
+//chiedi nome
+const firstName = prompt('Scrivi il tuo nome');
+console.log(firstName);
+
+//chiedi cognome
+const surname = prompt('Scrivi il tuo cognome');
+console.log(surname);
+
+//chiedi colore preferito
+const favColor = prompt('Qual è il tuo colore preferito?');
+console.log(favColor);
+
+//scrivi sulla pagina nomecognomecolorepreferito21
 
 const psw = document.getElementById('my_psw');
 
-const name = prompt('Scrivi il tuo nome');
-const surname = prompt('Scrivi il tuo cognome');
-const favColor = prompt('Qual è il tuo colore preferito?');
+psw.innerHTML = 'Password consigliata: ' + firstName + surname + favColor + '21';
 
-psw.innerHTML = 'Password consigliata: ' + name + surname + favColor + 21;
+console.log(psw);
+console.log(psw.innerHTML);
